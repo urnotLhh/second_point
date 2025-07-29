b16e4a01a049d4d47fd8a57962f2985c582c6613
urnotLhh <383306755@qq.com>
2025-07-29 23:22:59 +0800

    修改

--- main_agent/src/main/java/org/graduate/server/MainServer.java
+++ main_agent/src/main/java/org/graduate/server/MainServer.java
@@ -3,6 +3,7 @@ package org.graduate.server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.graduate.server.RagAgentServer;  // 确保包路径与实际定义一致
 
 @Slf4j
 @Component
