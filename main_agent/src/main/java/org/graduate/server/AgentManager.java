b54d352ce2720f562435462d1131dd1888b761c9
urnotlhh <qiguaidemeng@126.com>
2025-07-30 08:26:51 +0800

    同步代码

--- main_agent/src/main/java/org/graduate/server/AgentManager.java
+++ main_agent/src/main/java/org/graduate/server/AgentManager.java
@@ -20,6 +20,22 @@ public class AgentManager {
         return baiduLogoServer.getLogoOwner(filePath);
     }
 
+    @Tool(description = "获取lable.txt中的设备类型标签，包含类型，厂商和型号")
+    String getIoTLables(int lineIndex){
+
+    }
+
+    @Tool(description = "将物联网设备类型的合理标签存储到本地")
+    void storgeValidLables(List<String> lables){
+
+
+    }
+
+    @Tool(description = "将不合理的物联网设备类型的签存储到本地")
+    void storgeValidLables(List<String> lables){
+
+
+    }
 
 
 }
